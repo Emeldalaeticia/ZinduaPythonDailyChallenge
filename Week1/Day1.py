8c9d45eb8991eb8c7d690587dd231d2a4a822859
Emeldalaeticia <emelda.owira@aiesec.net>
2023-04-28 04:50:16 +0300

    deleted the file

--- Week1/Day1.py
+++
@@ -1,9 +0,0 @@
-#The program prints the prime numbers between 0 to 1000
-
-for num in range(1000):
-    if num > 1:
-        for i in range (2, num):
-            if (num % i) == 0:
-               break
-        else:
-                print(num)
